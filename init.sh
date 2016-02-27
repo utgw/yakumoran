e8c1e0b65c59e0732c6b259ee7b14d1b0b8aeb4f
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-02-27 22:18:31 +0900

    Add install package

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install vim git zsh
+sudo apt-get -y install vim git zsh tmux
+sudo apt-get -y install libbz2-dev libsqlite3-dev sqlite3
